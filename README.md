539c94db4ed7f200f9379d44700ad9a37a7e5611
Jay Gabriels <jgabriels30@gmail.com>
2018-05-19 18:10:28 -0700

    use client/server

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## Commands
 
+## Generate protobufs
 - $ protoc --go_out=. todo.proto
 - $ cat mydb.pd | protoc --decode_raw
 - $ hexdump -c mydb.pd
+
+## Generate grpc
+- $ protoc -I . todo.proto --go_out=.
+- $ protoc -I . todo.proto --go_out=plugins=grpc:.
+
+## Use
+cd /cmd/server/ && go build
+go run main.go list (fix later but server needs access to db)
+go run main.go add some todo to do
---
+++ cmd/server/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"github.com/gabrie30/protocol_buffers/todo"
+	"github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+)
+
+type taskServer struct{}
+type length int64
+
+const (
+	dbPath       = "mydb.pd"
+	sizeOfLength = 8
+)
+
+var endianness = binary.LittleEndian
+
+func (s taskServer) List(ctx context.Context, void *todo.Void) (*todo.TaskList, error) {
+	b, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read this file %s: %v", dbPath, err)
+	}
+
+	var tasks todo.TaskList
+	for {
+		if len(b) == 0 {
+			return &tasks, nil
+		} else if len(b) < sizeOfLength {
+			return nil, fmt.Errorf("remaining odd %d bytes len: ", len(b))
+		}
+
+		var l length
+
+		if err := binary.Read(bytes.NewReader(b[:sizeOfLength]), endianness, &l); err != nil {
+			return nil, fmt.Errorf("could not decode message length: %v", err)
+		}
+
+		b = b[sizeOfLength:]
+
+		var task todo.Task
+
+		if err := proto.Unmarshal(b[:l], &task); err != nil {
+			return nil, fmt.Errorf("could not read task: %v", err)
+		}
+
+		b = b[l:]
+		tasks.Tasks = append(tasks.Tasks, &task)
+
+		if task.Done {
+			fmt.Printf("[X] ")
+		} else {
+			fmt.Printf("[ ] ")
+		}
+
+		fmt.Println(task.Text)
+	}
+}
+
+func main() {
+	srv := grpc.NewServer()
+	var tasks taskServer
+	todo.RegisterTasksServer(srv, tasks)
+	l, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("could not list to :8888: %v", err)
+	}
+
+	log.Fatal(srv.Serve(l))
+}
+
+func (s taskServer) Add(ctx context.Context, text *todo.Text) (*todo.Task, error) {
+	task := &todo.Task{
+		Text: text.Text,
+		Done: false,
+	}
+
+	b, err := proto.Marshal(task)
+	if err != nil {
+		return nil, fmt.Errorf("could not encode task %+v", err)
+	}
+
+	f, err := os.OpenFile(dbPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %v", dbPath, err)
+	}
+
+	if err := binary.Write(f, endianness, length(len(b))); err != nil {
+		return nil, fmt.Errorf("cound not encode length of message %v", err)
+	}
+
+	_, err = f.Write(b)
+	if err != nil {
+		return nil, fmt.Errorf("could not write task to file: %v", err)
+	}
+
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close file %s: %v", dbPath, err)
+	}
+
+	return task, nil
+}
